12466654a0756b1507c5227feb369f41c31feead
Nicolás Castañeda <nicolascasol@unisabana.edu.co>
2024-02-25 19:38:38 -0500

    Contactos clickeables con buena interfaz

--- app/src/main/java/com/example/gestiondecontactos/ui/views/DetalleContactoScreen.kt
+++ app/src/main/java/com/example/gestiondecontactos/ui/views/DetalleContactoScreen.kt
@@ -1,8 +1,41 @@
 package com.example.gestiondecontactos.ui.views
 
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import com.example.gestiondecontactos.ui.viewModels.DetalleViewModel
 
-fun DetalleContacto(detalleViewModel: DetalleViewModel, navController: NavController){
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun DetalleContacto(detalleViewModel: DetalleViewModel, navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Volver") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    )
+    {
+
+    }
+}
+
+@Composable
+fun volver() {
 
 }
--- app/src/main/java/com/example/gestiondecontactos/ui/views/MS.kt
+++ app/src/main/java/com/example/gestiondecontactos/ui/views/MS.kt
@@ -1,12 +1,15 @@
 package com.example.gestiondecontactos.ui.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +17,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.ArrowForward
@@ -27,10 +32,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,8 +46,10 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.example.gestiondecontactos.Contacto
+import com.example.gestiondecontactos.R
 import com.example.gestiondecontactos.ui.viewModels.MSViewModel
 import kotlinx.coroutines.flow.Flow
+import java.time.format.TextStyle
 
 @Composable
 fun MainScreen(msViewModel: MSViewModel, navController: NavController) {
@@ -47,13 +57,24 @@ fun MainScreen(msViewModel: MSViewModel, navController: NavController) {
     val contactosState by msViewModel.obtenerContactos().collectAsState(initial = emptyList())
 
     Column {
-        Text(text = "CONTACTOS")
-        Box(modifier = Modifier.fillMaxSize().weight(0.8f)) {
+        Text(
+            text = "CONTACTOS",
+            modifier = Modifier.padding(15.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.8f),
+            contentAlignment = Alignment.BottomStart
+        ) {
             listaContactos(navController, contactosState)
+            agregarContactoButton(onClick = { showAddContactDialog.value = true })
         }
 
 
-        agregarContactoButton(onClick = { showAddContactDialog.value = true })
+
 
 
 
@@ -84,17 +105,31 @@ fun listaContactos(navController: NavController, contactos: List<Contacto>) {
 
 @Composable
 fun ContactoItem(navController: NavController, contacto: Contacto) {
-    Column(modifier = Modifier.padding(16.dp)
-        .clickable { navController.navigate("detalleContacto/${contacto}") }
+    Column(modifier = Modifier
+
+        .padding(15.dp, 5.dp, 15.dp, 0.dp)
+        .clickable { navController.navigate("detalleContacto") }
+        .fillMaxWidth()
+        .background(Color(237, 244, 244), shape = RoundedCornerShape(5.dp))
     ) {
-        Text("Nombre: ${contacto.nombre}")
-        Text("Teléfono: ${contacto.telefono}")
+        Box(modifier = Modifier.padding(10.dp)) {
+            Column {
+                Row{
+                    Image(painterResource(R.drawable.perfilcontactos), "Foto_Perfil", Modifier.size(50.dp).padding(0.dp, 0.dp, 10.dp, 0.dp))
+                    Column {
+                        Text("${contacto.nombre}", fontWeight = FontWeight.Bold)
+                        Text("${contacto.telefono}")
+                    }
+                }
+               
+            }
+        }
     }
 }
 
 @Composable
 fun agregarContactoButton(onClick: () -> Unit, icon: ImageVector = Icons.Rounded.Add) {
-    Button(onClick = onClick) {
+    Button(onClick = onClick, modifier = Modifier.padding(15.dp)) {
         Icon(icon, contentDescription = null, modifier = Modifier.padding(0.dp, 0.dp, 10.dp, 0.dp))
         Text(text = "Agregar Contacto")
     }
